da8efe5a9fa996be565e402a82eb747f1e0fffef
Mohamed Rismi Rukshan <rukshanlancer@gmail.com>
2022-11-02 12:01:16 +0800

    youtube video search app

---
+++ src/apis/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyAJpqck9YuPlnrTX8Dss4HvWo51Kata82A';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params:{
+        part: 'snippet',
+        maxResults: 5,
+        type: 'video',
+        key: KEY
+    }
+});
