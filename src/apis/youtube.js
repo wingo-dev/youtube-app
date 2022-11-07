f13f116b1ab192ee30ebb99873c2bd17227e4de5
Mohamed Rismi Rukshan <rukshanlancer@gmail.com>
2022-11-07 19:39:51 +0800

    build complete

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -6,8 +6,8 @@ export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params:{
         part: 'snippet',
-        maxResults: 5,
+        maxResults: 15,
         type: 'video',
-        key: KEY
+        key: KEY,
     }
 });
